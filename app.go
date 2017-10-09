15d8d442511e2a99bcf53e88c63aea7352b7a190
Henrique Oliveira <haooliveira@gmail.com>
2017-10-09 18:05:17 -0300

    Add feat to export json

--- app.go
+++ app.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"os"
 )
+
+type Person struct {
+	State    string `json:"State"`
+	City     string `json:"City"`
+	Fullname string `json:"FullName"`
+}
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Missing parameter")
 		return
 	}
-	var filename = string(os.Args[1])
-	file, err := ioutil.ReadFile(filename)
+	csvFile, err := os.Open(os.Args[1])
+	file := csv.NewReader(bufio.NewReader(csvFile))
 	if err != nil {
-		fmt.Println("Can't read file", filename)
+		fmt.Println("Can't read file")
 		panic(err)
 	}
-	fmt.Println(string(file))
+	var people []Person
+	for {
+		row, error := file.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		people = append(people, Person{
+			State:    row[0],
+			City:     row[1],
+			Fullname: row[2],
+		})
+	}
+	peopleJson, _ := json.Marshal(people)
+	fmt.Println(string(peopleJson))
 }
